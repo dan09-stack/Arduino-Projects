87748693914e223fb83b132f70f6443aaa412623
dan09 <daro.albis.up@phinmaed.com>
2024-07-13 16:03:35 +0800

    updated activity2

--- Activity-2.ino
+++ Activity-2.ino
@@ -1,33 +1,18 @@
-//LED Chasser
-int ledPin1 = 12;
-int ledPin2 = 11;
-int ledPin3 = 10;
-int ledPin4 = 9;
-int ledPin5 = 8;
+//LED Chaser
+const int ledPins[] = {12, 11, 10, 9, 8};
+const int numLeds = 5;
+const int delayTime = 1000;
 
-int time = 1000;
-
-void setup(){
-    pinMode(ledPin1, OUTPUT);
-    pinMode(ledPin2, OUTPUT);
-    pinMode(ledPin3, OUTPUT);
-    pinMode(ledPin4, OUTPUT);
-    pinMode(ledPin5, OUTPUT);
+void setup() {
+  for (int i = 0; i < numLeds; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
 }
-void loop(){
-    digitalWrite(ledPin1, HIGH);
-    delay(time);
-    digitalWrite(ledPin1, LOW);
-    digitalWrite(ledPin2, HIGH);
-    delay(time);
-    digitalWrite(ledPin2, LOW);
-    digitalWrite(ledPin3, HIGH);
-    delay(time);
-    digitalWrite(ledPin3, LOW);
-    digitalWrite(ledPin4, HIGH);
-    delay(time);
-    digitalWrite(ledPin4, LOW);
-    digitalWrite(ledPin5, HIGH);
-    delay(time);
-    digitalWrite(ledPin5, LOW);
+
+void loop() {
+  for (int i = 0; i < numLeds; i++) {
+    digitalWrite(ledPins[i], HIGH);
+    delay(delayTime);
+    digitalWrite(ledPins[i], LOW);
+  }
 }
